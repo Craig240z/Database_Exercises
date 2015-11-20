27cfdc67ed71ff2fef277ccf1e6bfd4d4dad4bb9
Craig Re <car.code240z@gmail.com>
2015-11-20 11:06:58 -0600

    Created table in sublime to run in Mysql.

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,3 +1,20 @@
 USE codeup_test_db;
 
-USE TABLE DROP IF EXISTS 'albums';
+DROP TABLE IF EXISTS albums;
+ 
+CREATE TABLE IF NOT EXISTS albums (
+
+    -- auto incrementing unsigned integer primary key
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    -- string for storing the recording artist name
+    artist VARCHAR(25) NOT NULL,
+    -- string for storing a record name
+    name VARCHAR(25) NOT NULL,
+    -- integer representing year record was released (Note: MySQL also supports a YEAR data type!)
+    release_date DATE, 
+    -- floating point value for number of records sold (in millions)
+    sales DECIMAL(8,2),
+    -- string for storing the record's genre(s)
+    genre VARCHAR(20),
+    PRIMARY KEY (id)
+);
