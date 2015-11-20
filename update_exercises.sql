e9c9344cdb58b74cd8e6717d957715bb3a907143
Craig Re <car.code240z@gmail.com>
2015-11-20 15:26:45 -0600

    Created update file and wrote first SELECT statement for All Albums by Michael Jackson.

--- update_exercises.sql
+++ update_exercises.sql
@@ -6,3 +6,7 @@ FROM albums;
 SELECT release_date AS "All Albums released before 1980"
 FROM albums
 WHERE release_date < 1980;
+
+SELECT name AS "All Albums by Michael Jackson"
+FROM albums
+WHERE artist = "Michael Jackson";
