f79ef7ec39b53c52aa8400d103567ac908402848
Craig Re <car.code240z@gmail.com>
2015-11-23 13:39:41 -0600

    First SQL pro file on where exercises.

---
+++ where_exercises.sql
@@ -0,0 +1,22 @@
+-- SELECT * FROM employees WHERE hire_date = '1985-01-03';
+
+SELECT	first_name
+FROM	employees
+WHERE first_name IN	('Irena' , 'Vidya' , 'Maya');
+
+SELECT last_name
+FROM employees	
+WHERE	last_name LIKE 'E%';
+
+SELECT	hire_date
+FROM	employees
+WHERE	hire_date BETWEEN	'1990-01-01' AND '1999-12-31';
+
+SELECT	birth_date
+FROM	employees
+WHERE	birth_date LIKE	'%-12-25';
+
+SELECT	last_name
+FROM	employees
+WHERE	last_name LIKE	'%q%';
+
