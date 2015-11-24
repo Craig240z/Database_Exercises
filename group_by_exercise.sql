4b837fd7d685e1eba1be8e7a72da725a97b46303
Craig Re <car.code240z@gmail.com>
2015-11-24 10:48:33 -0600

    Created group by exercise and modified from the where exercise, tested each in the GUI.

---
+++ group_by_exercise.sql
@@ -0,0 +1,23 @@
+
+SELECT DISTINCT title
+FROM titles;
+
+SELECT DISTINCT title
+FROM titles DESC;
+
+SELECT last_name
+FROM employees  
+WHERE   last_name LIKE 'E%'
+AND last_name LIKE '%E'
+GROUP BY last_name;
+
+SELECT first_name, last_name
+FROM employees  
+WHERE   last_name LIKE 'E%'
+AND last_name LIKE '%E'
+GROUP BY first_name, last_name;
+
+SELECT  DISTINCT last_name
+FROM    employees
+WHERE   last_name LIKE  '%q%'
+AND last_name  NOT LIKE '%qu%';
